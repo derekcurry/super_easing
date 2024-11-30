4b21116b070a64efe9c813366419c62c5b897050
Derek Curry <derekcur@buffalo.edu>
2024-11-30 10:48:30 -0500

    Update change_speed.ino
    Updated example for definitions of easing options.

--- examples/change_speed/change_speed/change_speed.ino
+++ examples/change_speed/change_speed/change_speed.ino
@@ -1,6 +1,6 @@
 /* ***** Super Easer *********************************
 
-  *** change speed demo *****
+  *** Joystick demo *****
 
   Servo easing library without using an Arduino delay 
 
@@ -41,51 +41,97 @@
 // Variables for not using a delay()
 // For a thorough explaination of this see: https://learn.adafruit.com/multi-tasking-the-arduino-part-1/ditch-the-delay
 unsigned long lastGlobalUpdate;
-long globalUpdateInterval = 5000;
+long globalUpdateInterval = 5;
 
-// Variable for the servo speed:
-int servo_speed = 1;
+// This code assumes that you are using a switch-based joystick
+// Joystick pins 
+#define RIGHT_PIN 2
+#define LEFT_PIN 3
+#define UP_PIN 4
+#define DOWN_PIN 5
+
+// Direction variables
+int left;
+int right;
+int up;
+int down;
+
 // Initialize the Easer (one for each servo)
-Easer my_servo(servo_speed, CUBIC, 2);
+Easer my_servo_1(5, CUBIC, 0);
+Easer my_servo_2(5, QUADRATIC, 1);
+/*****************************************************************
+    Easer class vairables:
+      (interval, type-of-easing, easing-motion, min-servo-position, max-servo-position, servo-midpoint)
+    interval is required (controls speed), all other variables are optional
+    The lower the speed-interval number, the faster the movement
+    The exact min, max, and midpoint can vary from servo to servo
+    The default easing type is: cubic if no variable is given
+    The default easing motion is: ease-out if no variable is given
+*******************************************************************/
 
 void setup() { 
   Serial.begin(115200);
   
-  my_servo.Attach(9);  // Attach the servo to pin 9
-  // *** Notice the capital 'A' in "Attach"
-  // Any pin capable of PWM can be used
-  
+  my_servo_1.Attach(12);  // Attach the servo to pin 9  ***Any pin capable of PWM can be used
+  my_servo_2.Attach(11);
+  //*** Notice the capital 'A' in "Attach"
+
+  // Joystick pins
+  pinMode(LEFT_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_PIN, INPUT_PULLUP);
+  pinMode(UP_PIN, INPUT_PULLUP);
+  pinMode(DOWN_PIN, INPUT_PULLUP);
 } 
  
- 
+// *** Using an Arduino delay() function will break the program, use update intervals
 void loop() { 
+  
   // Use a global update variable to avoid using a delay()
-  if((millis() - lastGlobalUpdate) > globalUpdateInterval) {
-    if(my_servo.dest == my_servo.sMax) {
-      my_servo.dest = my_servo.sMin;
-      // slow the servo down by increasing the update interval --> reset at 20
-      if(servo_speed < 20){
-        servo_speed += 1;
-      } else {
-        servo_speed = 1;
-      }
-      my_servo.updateInterval = servo_speed;
-      Serial.println(servo_speed);
-    } else {
-      my_servo.dest = my_servo.sMax;
-      // slow the servo down by increasing the update interval --> reset at 20
-      if(servo_speed < 20){
-        servo_speed += 1;
-      } else {
-        servo_speed = 1;
-      }
-      my_servo.updateInterval = servo_speed;
-      Serial.println(my_servo.updateInterval);
-    }
+  if ((millis() - lastGlobalUpdate) >= globalUpdateInterval) {
     lastGlobalUpdate = millis();
+    //Serial.println("Still Running....");
     
-  }
-  // You must update the servo for it to move
-  my_servo.Update();
+    left = digitalRead(LEFT_PIN); 
+    right = digitalRead(RIGHT_PIN);
+    up = digitalRead(UP_PIN);
+    down = digitalRead(DOWN_PIN);
 
+    /* 
+      Change the dest of the servo depending on the joystick
+      Servos move to the midpoint if no direction is pressed
+    */ 
+    // ---- Left & Right----
+    if((left==1)&&(right==1)) {
+      // Go to center
+      Serial.print(" center ");
+      my_servo_1.dest = my_servo_1.sMid;
+    } else {
+      if(left==0){
+        Serial.print(" left ");
+        my_servo_1.dest = my_servo_1.sMax;
+      } else {
+        Serial.print(" right ");
+        my_servo_1.dest = my_servo_1.sMin;
+      }
+    }
+    // ---- Up & Down----
+    if((up==1)&&(down==1)){
+      // Go to the middle
+      Serial.print(" middle ");
+      my_servo_2.dest = my_servo_2.sMid;
+    } else {
+      if(up==0){
+        Serial.print(" up ");
+        my_servo_2.dest = my_servo_2.sMax;
+      } else {
+        Serial.print(" down ");
+        my_servo_2.dest = my_servo_2.sMin;
+      }
+    }
+    Serial.println();
+  } // end of global timer loop
+
+  // You must update each servo for them to move
+  my_servo_1.Update();
+  my_servo_2.Update();
 }
